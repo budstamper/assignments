af46e7605a687dca7150ece5a9e9b7d582e1f3a1
Bud Stamper <budstamper89@gmail.com>
2018-07-18 22:17:19 -0400

    initial comit from video

---
+++ grocery-store.js
@@ -0,0 +1,9 @@
+var shopper(nameCust, num, hasMoney, ) {
+    var name: nameCust,
+    var customerNumber: num,
+    var hasMoney : true,
+    cartItems: ["bananas", "hot sauce", "chips"],
+    function buyStuff(){
+        
+    }
+}
